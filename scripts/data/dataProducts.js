1905a637ca27265a3a702d179b51583da73ccdbc
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2025-01-07 00:05:59 +0530

    .

---
+++ scripts/data/dataProducts.js
@@ -0,0 +1,131 @@
+const pizza = [
+    {
+        id : "pizza 01",
+        image : "images/margirita.png",
+        name : "Margherita Pizza",
+        disc : "a classic italian pizza topped with fresh mozzarella and tangy tomato sause.",
+        price : 250
+    },
+    {
+        id : "pizza 02",
+        image : "images/onion_pizza.png",
+        name : "Onion Pizza",
+        disc : "Topped with caramelized onions, melted cheese, and a rich tomato base.",
+        price : 300
+    },
+    {
+        id : "pizza 03",
+        image : "images/Pepperoni_pizza.png",
+        name : "Pepperoni Pizza",
+        disc : "Topped with pepperoni slices,mozzarella cheese, and a flavorful tomato sauce.",
+        price : 400
+    },
+    {
+        id : "pizza 04",
+        image : "images/veggie_pizza.png",
+        name : "Veggie Pizza",
+        disc : "A mix of vegetables such as bell peppers, onions, mushrooms, and olives.",
+        price : 350
+    },
+    {
+        id : "pizza 05",
+        image : "images/margirita.png",
+        name : "Margherita Pizza",
+        disc : "a classic italian pizza topped with fresh mozzarella and tangy tomato sause.",
+        price : 250
+    },
+    {
+        id : "pizza 06",
+        image : "images/onion_pizza.png",
+        name : "Onion Pizza",
+        disc : "Topped with caramelized onions, melted cheese, and a rich tomato base.",
+        price : 300
+    },
+    {
+        id : "pizza 07",
+        image : "images/Pepperoni_pizza.png",
+        name : "Pepperoni Pizza",
+        disc : "Topped with pepperoni slices,mozzarella cheese, and a flavorful tomato sauce.",
+        price : 400
+    },
+    {
+        id : "pizza 08",
+        image : "images/veggie_pizza.png",
+        name : "Veggie Pizza",
+        disc : "A mix of vegetables such as bell peppers, onions, mushrooms, and olives.",
+        price : 350
+    },
+    {
+        id : "pizza 05",
+        image : "images/margirita.png",
+        name : "Margherita Pizza",
+        disc : "a classic italian pizza topped with fresh mozzarella and tangy tomato sause.",
+        price : 250
+    },
+    {
+        id : "pizza 06",
+        image : "images/onion_pizza.png",
+        name : "Onion Pizza",
+        disc : "Topped with caramelized onions, melted cheese, and a rich tomato base.",
+        price : 300
+    },
+    {
+        id : "pizza 07",
+        image : "images/Pepperoni_pizza.png",
+        name : "Pepperoni Pizza",
+        disc : "Topped with pepperoni slices,mozzarella cheese, and a flavorful tomato sauce.",
+        price : 400
+    },
+    {
+        id : "pizza 08",
+        image : "images/veggie_pizza.png",
+        name : "Veggie Pizza",
+        disc : "A mix of vegetables such as bell peppers, onions, mushrooms, and olives.",
+        price : 350
+    }
+];
+
+const burger = [
+    {
+        id : "burger 01",
+        image : "images/burger.png",
+        name : "Classic Burger",
+        disc : "A classic burger is a juicy beef patty with fresh toppings in a soft bun.",
+        price : 99
+    },
+    {
+        id : "burger 02",
+        image : "images/aloo-tikki-burger.png",
+        name : "Aloo Tikki Burger",
+        disc : "A Aloo Tikki Burger is spiced potato patty in a bun with veggies and sauces.",
+        price : 109
+    },
+    {
+        id : "burger 03",
+        image : "images/chickenBurger.png",
+        name : "Chicken Burger",
+        disc : "A chicken burger is a bun sandwich with a chicken patty and toppings.",
+        price : 119
+    },
+    {
+        id : "burger 01",
+        image : "images/burger.png",
+        name : "Classic Burger",
+        disc : "A classic burger is a juicy beef patty with fresh toppings in a soft bun.",
+        price : 99
+    },
+    {
+        id : "burger 02",
+        image : "images/aloo-tikki-burger.png",
+        name : "Aloo Tikki Burger",
+        disc : "A Aloo Tikki Burger is spiced potato patty in a bun with veggies and sauces.",
+        price : 109
+    },
+    {
+        id : "burger 03",
+        image : "images/chickenBurger.png",
+        name : "Chicken Burger",
+        disc : "A chicken burger is a bun sandwich with a chicken patty and toppings.",
+        price : 119
+    }
+];
